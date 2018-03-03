be6975cfeaf7f7accbd4a389bc65ac2163481524
1481078 <smallvu2001@gmail.com>
2018-03-03 13:21:54 +0700

    Update Cong_ty.xml và 3A-Mo_hinh_Doi_tuong.cs

--- Cong_ty_phan_phoi_Sua_va_Banh_rong_bien/Dich_vu_Ban_Banh_rong_bien/Dich_vu/3-Doi_tuong_va_Xu_ly/3A-Mo_hinh_Doi_tuong.cs
+++ Cong_ty_phan_phoi_Sua_va_Banh_rong_bien/Dich_vu_Ban_Banh_rong_bien/Dich_vu/3-Doi_tuong_va_Xu_ly/3A-Mo_hinh_Doi_tuong.cs
@@ -5,35 +5,87 @@ using System.Web;
 
 #region "********Dữ liệu Ứng dụng ***********"
 // Du_lieu:
-//   Cua_hang:Ma_so,Ten
-//     Danh_sach_Nhom_Dien_thoai:
-//       * Nhom_Dien_thoai:Ma_so,Ten
+//   Cong_ty:Ma_so,Ten
+//     Danh_sach_Nhom_San_pham:
+//       * Nhom_San_pham:Ma_so,Ten
 //     Danh_sach_Nhan_vien:
 //       * Nhan_vien :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
 //          Nhom_Nhan_vien:Ma_so,Ten
 //     Danh_sach_Quan_ly:
 //       * Quan_ly :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
 //          Nhom_Quan_ly:Ma_so,Ten
-//   Danh_sach_Dien_thoai: 
-//      * Dien_thoai :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap
-//          Nhom_Dien_thoai:Ma_so,Ten
+//   Danh_sach_San_pham: 
+//      * San_pham :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap
+//          Nhom_San_pham:Ma_so,Ten
 //          Danh_sach_Ban_hang:
-//            * Ban_hang : Ngay,So_luong,Don_gia,Tien
+//            * Ban_hang : Ngay,So_luong,Don_gia,Tien, Giao_hang
 //          Danh_sach_Nhap_hang:
 //            * Nhap_hang : Ngay,So_luong,Don_gia,Tien
 #endregion
 
 #region "********Dữ liệu của Phân hệ Khách Tham quan ***********"
 //############### Lưu ý :Có bổ sung
-//  Số lượng Tồn của Điện thoại 
+//  Số lượng Tồn của Sản phẩm 
 //###############
 // Du_lieu:
-//   Cua_hang:Ma_so,Ten
-//   Danh_sach_Dien_thoai: 
-//      * Dien_thoai :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton
+//   Cong_ty:Ma_so,Ten
+//   Danh_sach_San_pham: 
+//      * San_pham :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton
 #endregion
 
-#region "********Dữ liệu của Phân hệ Nhân viên Nhập hàng ***********"
+#region "********Dữ liệu của Phân hệ Nhân viên Bán hàng ***********"
+//############### Lưu ý :Có bổ sung
+//  Số lượng Tồn,Doanh thu của Sản phẩm 
+//###############
+// Du_lieu:
+//   Cong_ty:Ma_so,Ten
+//     Danh_sach_Nhom_San_pham:
+//       * Nhom_San_pham:Ma_so,Ten
+//     Danh_sach_Nhan_vien:
+//       * Nhan_vien :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau,Doanh_thu
+//          Nhom_Nhan_vien:Ma_so,Ten
+//     Danh_sach_Quan_ly:
+//       * Quan_ly :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
+//          Nhom_Quan_ly:Ma_so,Ten
+//   Danh_sach_San_pham: 
+//      * San_pham :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton,Doanh_thu
+#endregion
+
+#region "********Dữ liệu của Phân hệ Nhân viên Giao hàng ***********"
+//############### Lưu ý :Có bổ sung
+// Du_lieu:
+//   Cong_ty:Ma_so,Ten
+//     Danh_sach_Nhom_San_pham:
+//       * Nhom_San_pham:Ma_so,Ten
+//     Danh_sach_Nhan_vien:
+//       * Nhan_vien :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau,Doanh_thu
+//          Nhom_Nhan_vien:Ma_so,Ten
+//     Danh_sach_Quan_ly:
+//       * Quan_ly :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
+//          Nhom_Quan_ly:Ma_so,Ten
+//   Danh_sach_San_pham: 
+//      * San_pham :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton,Doanh_thu
+#endregion
+
+#region "********Dữ liệu của Phân hệ Quản lý Bán hàng ***********"
+//############### Lưu ý :Có bổ sung
+//  Số lượng Tồn,Doanh thu của Sản phẩm 
+//###############
+// Du_lieu:
+//   Cong_ty:Ma_so,Ten
+//     Danh_sach_Nhom_San_pham:
+//       * Nhom_San_pham:Ma_so,Ten
+//     Danh_sach_Nhan_vien:
+//       * Nhan_vien :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau,Doanh_thu
+//          Nhom_Nhan_vien:Ma_so,Ten
+//     Danh_sach_Quan_ly:
+//       * Quan_ly :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
+//          Nhom_Quan_ly:Ma_so,Ten
+//   Danh_sach_San_pham: 
+//      * San_pham :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton,Doanh_thu
+#endregion
+
+#region "********Dữ liệu của Phân hệ Quản lý Kho ***********"
 //############### Lưu ý :Có bổ sung
 //  Số lượng Tồn của Điện thoại 
 //###############
@@ -51,36 +103,22 @@ using System.Web;
 //      * Dien_thoai :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton
 #endregion
 
-#region "********Dữ liệu của Phân hệ Nhân viên Bán hàng ***********"
+#region "********Dữ liệu của Phân hệ Quản lý Công ty ***********"
 //############### Lưu ý :Có bổ sung
-//  Số lượng Tồn,Doanh thu của Điện thoại 
+//  Số lượng Tồn,Doanh thu của Sản phẩm 
 //###############
 // Du_lieu:
-//   Cua_hang:Ma_so,Ten
-//     Danh_sach_Nhom_Dien_thoai:
-//       * Nhom_Dien_thoai:Ma_so,Ten
+//   Cong_ty:Ma_so,Ten
+//     Danh_sach_Nhom_San_pham:
+//       * Nhom_San_pham:Ma_so,Ten
 //     Danh_sach_Nhan_vien:
 //       * Nhan_vien :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau,Doanh_thu
 //          Nhom_Nhan_vien:Ma_so,Ten
 //     Danh_sach_Quan_ly:
 //       * Quan_ly :Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
 //          Nhom_Quan_ly:Ma_so,Ten
-//   Danh_sach_Dien_thoai: 
-//      * Dien_thoai :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton,Doanh_thu
-#endregion
-#region "********Dữ liệu của Phân hệ Quản lý Bán hàng ***********"
-// Du_lieu:
-//   Cua_hang:Ma_so,Ten
-//     Danh_sach_Nhom_Dien_thoai:
-//       * Nhom_Dien_thoai: Ma_so, Ten, So_luong_ton, Doanh_thu 
-//     Danh_sach_Nhan_vien_Ban_hang:
-//       * Nhan_vien: Ma_so, Ho_ten, Ten_Dang_nhap, Mat_khau, Doanh_thu
-//          Nhom_Nhan_vien: Ma_so, Ten
-//     Danh_sach_Quan_ly:
-//       * Quan_ly: Ma_so,Ho_ten,Ten_Dang_nhap,Mat_khau
-//          Nhom_Quan_ly: Ma_so,Ten
-//   Danh_sach_Dien_thoai: 
-//      * Dien_thoai :Ma_so, Ten, Don_gia_Ban, So_luong_Ton, Doanh_thu
+//   Danh_sach_San_pham: 
+//      * San_pham :Ma_so,Ten,Don_gia_Ban,Don_gia_Nhap, So_luong_Ton,Doanh_thu
 #endregion
 
 
